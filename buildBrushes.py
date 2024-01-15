419e658f6623038e4f8ed2ce3f0bf9227b77456f
dfgordon <dfgordon@me.com>
2024-01-14 19:30:19 -0500

    faster TRAP and STROKE

--- buildBrushes.py
+++ buildBrushes.py
@@ -1,4 +1,6 @@
-'''Metaprogram creating HEX pseudo-ops representing elliptical brushes.'''
+'''Metaprogram creating HEX pseudo-ops representing elliptical brushes.
+The brush is described as a set of hlines.  It is important
+therefore that the library have an efficient hline routine.'''
 
 # this has the elliptical shapes (not found to be so useful)
 #grids = [(4,3),(8,4),(12,5),(18,8),(24,10),(36,15),(48,20),(12,10),(24,20),(24,4),(48,8)]
@@ -8,7 +10,14 @@ grids = [(4,3),(8,4),(12,5),(18,8),(24,10),(36,15),(48,20)]
 
 asmStr = ''
 
-def hc(x,bits):
+class hlin:
+    offset = 0
+    run = 0
+    def __str__(self):
+        return str(self.offset) + "," + str(self.run)
+
+def hc(x: int,bits: int) -> str:
+    '''hex string representing `x` with padding to encompass requested bit count'''
     ans = hex(x)[2:]
     if len(ans)>int(bits/4):
         raise ValueError('cannot represent '+ans+' with '+str(bits)+' bits')
@@ -21,40 +30,47 @@ def ellipse(xp,yp,a,b):
         return 1
     return 0
 
-offset = 0
-asmStr += '* following are big endian' + '\n'
-asmStr += 'brushTbl HEX   '
-for b in grids:
-    asmStr += hc(offset,16)
-    offset += 2 + b[1]*(1+int((b[0]-1)/8))
-asmStr += '\n'
+brushes = []
 
-for b in grids:
-    xend = b[0]
-    yend = b[1]
-    if xend==grids[0][0] and yend==grids[0][1]:
-        line = 'brushes  HEX   ' + hc(xend,8) + hc(yend,8)
-    else:
-        line = '         HEX   ' + hc(xend,8) + hc(yend,8)
+for (xend,yend) in grids:
+    brushes += [[]]
     a = (xend-1)/2
     b = (yend-1)/2
-    print(a,b)
     for y in range(yend):
-        val = 0
-        bits = 0
+        line = hlin()
+        running = False
         for x in range(xend):
-            val += 2**(7-bits)*ellipse(x-a,y-b,a+1,b+1)
-            bits += 1
-            if bits==8:
-                line += hc(val,8)
-                val = 0
-                bits = 0
-            if len(line)>58:
-                asmStr += line + '\n'
-                line = '         HEX   '
-        if bits>0:
-            line += hc(val,8)
-    if line[-1]!=' ':
-        asmStr += line + '\n'
+            # do it this way to keep new brushes same as old brushes
+            if (not running) and ellipse(x-a,y-b,a+1,b+1)==1:
+                line.offset = x
+                running = True
+            if running and ellipse(x-a,y-b,a+1,b+1)==0:
+                line.run = x - line.offset
+                running = False
+                break
+        if running:
+            line.run = xend - line.offset
+        brushes[-1] += [line]
+
+asmStr += 'brushTbl\n'
+offset = 0
+asmStr += '         HEX   '
+for b in brushes:
+    asmStr += hc(offset,8) + ','
+    offset += 2*len(b) + 1
+asmStr = asmStr[:-1]
+asmStr += '\n'
+
+asmStr += 'brushes\n'
+for b in brushes:
+    asmStr += '         HEX   ' + hc(len(b),8) + '\n'
+    asmStr += '         HEX   '
+    for l in b:
+        asmStr += hc(l.offset,8)
+    asmStr += '\n'
+    asmStr += '         HEX   '
+    for l in b:
+        asmStr += hc(l.run,8)
+    asmStr += '\n'
 
 print(asmStr)
